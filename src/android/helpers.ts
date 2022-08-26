46e559885c00b2da0d6f84b738ff60bbca4540f8
Jeffrey de Looff <jeffrey.delooff@elements.nl>
2022-08-26 09:22:07 +0200

    refactor: naming

--- src/android/helpers.ts
+++ src/android/helpers.ts
@@ -1,6 +1,5 @@
-import { readFileSync } from 'fs'
 import { fileToJSON } from '../common/helpers'
 import { AndroidJsonOptions } from './types'
 
-export const readOptionsAsJson = (configPath: string) =>
+export const readJsonFileAsOptions = (configPath: string) =>
   fileToJSON(configPath) as AndroidJsonOptions
